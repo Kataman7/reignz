1500d2953a7b01ce0fc9bf4f21be049d534ac3cd
Antonin <antonin.chabaud-pech@etu-montpellier.fr>
2024-03-16 23:45:51 +0100

    script création de table

--- src/script.sql
+++ src/script.sql
@@ -1,53 +1,59 @@
-CREATE TABLE balance (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    economie INT,
-    religion INT,
-    militaire INT,
+CREATE TABLE paquet
+(
+    id   INT AUTO_INCREMENT PRIMARY KEY,
+    poid INT
+);
+
+CREATE TABLE balance
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    economie   INT,
+    religion   INT,
+    militaire  INT,
     population INT,
-    science INT
+    science    INT
 );
 
-CREATE TABLE consequence (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE consequence
+(
+    id     INT AUTO_INCREMENT PRIMARY KEY,
     cartes INT,
-    FOREIGN KEY (cartes) REFERENCES paquet(id)
+    FOREIGN KEY (cartes) REFERENCES paquet (id)
 );
 
-CREATE TABLE choix (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE choix
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
     description VARCHAR(400),
     consequence INT,
-    FOREIGN KEY (consequence) REFERENCES consequence(id)
+    FOREIGN KEY (consequence) REFERENCES consequence (id)
 );
 
-CREATE TABLE carte (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE carte
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
     description VARCHAR(400),
-    image VARCHAR(150),
+    image       VARCHAR(150),
     choixGauche INT,
     choixDroite INT,
-    FOREIGN KEY (choixGauche) REFERENCES choix(id),
-    FOREIGN KEY (choixDroite) REFERENCES choix(id)
+    FOREIGN KEY (choixGauche) REFERENCES choix (id),
+    FOREIGN KEY (choixDroite) REFERENCES choix (id)
 );
 
-CREATE TABLE paquet (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    poid INT,
-);
-
-CREATE TABLE appartenance (
+CREATE TABLE appartenance
+(
     paquet INT,
-    carte INT,
-    FOREIGN KEY (paquet) REFERENCES paquet(id),
-    FOREIGN KEY (carte) REFERENCES carte(id)
-)
+    carte  INT,
+    FOREIGN KEY (paquet) REFERENCES paquet (id),
+    FOREIGN KEY (carte) REFERENCES carte (id)
+);
 
-CREATE TABLE jeu (
-    balance INT,
-    pioche INT,
+CREATE TABLE jeu
+(
+    balance       INT,
+    pioche        INT,
     carteSuivante INT,
-    FOREIGN KEY (balance) REFERENCES balance(id),
-    FOREIGN KEY (pioche) REFERENCES paquet(id),
-    FOREIGN KEY (carteSuivante) REFERENCES paquet(id),
-)
-
+    FOREIGN KEY (balance) REFERENCES balance (id),
+    FOREIGN KEY (pioche) REFERENCES paquet (id),
+    FOREIGN KEY (carteSuivante) REFERENCES paquet (id)
+);
